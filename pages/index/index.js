7ec13b4a9201df4fed80d1a9191943c3c01e4a54
Yangkai.Shen <237497819@qq.com>
2019-11-07 17:55:36 +0800

    :fire: 移除 log 页面，打印 log 查看用户信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,10 +10,8 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  bindViewTap: function () {
+    console.log(app.globalData.userInfo)
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -21,10 +19,11 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
+        console.log(res)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
@@ -43,7 +42,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
